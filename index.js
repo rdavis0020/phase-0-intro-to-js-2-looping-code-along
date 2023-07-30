ff5b5ed22fb5dd9b6ed475ca84540f583daf84f8
Ryan Davis <rdavis0020@gmail.com>
2023-07-30 17:32:50 -0400

    Add File

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  let i = 0; 
+  while (i < gifts.length) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    i++; 
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(namesArray, eventName) {
+  const thankYouMessages = [];
+
+  for (let i = 0; i < namesArray.length; i++) {
+    const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+    thankYouMessages.push(message);
+  }
+
+  return thankYouMessages;
+
+}
+const startingNumber = 10;
+countDown(startingNumber);
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
